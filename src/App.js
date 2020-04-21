35cabd703880320c3c26af47dc1fc5f5896b835b
Rômulo Rocha <romulorocha063@gmail.com>
2020-04-21 20:00:37 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from 'services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+      title: `Mikonos API  ${Date.now()}`, 
+	    url:"https://github.com",
+	    techs:["NodeJs", "git"]
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+
+    const findIndex = repositories.findIndex(repo => repo.id === id)
+    const newList = [...repositories]
+    newList.splice(findIndex,1)
+
+    setRepositories(newList)
   }
+
+  useEffect(()=>{
+    (async function loadRepositories() {
+      const { data } = await api.get('/repositories');
+
+      setRepositories(data)
+    })()
+  },[])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repo => (
+              <li key={repo.id}>
+                {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+                <button onClick={() => handleRemoveRepository(repo.id)}>
+                  Remover
+                </button>
+              </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
